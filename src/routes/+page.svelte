30bde03a86851b1385692eaca69c2e8356335e63
Nathan Roark <nathanroark@protonmail.com>
2025-03-13 20:12:00 -0500

    idk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,19 @@
-<div>
+<div class="p-4">
 	<h1 class="text-2xl">Stylehero</h1>
 	<p>Challenges built to test your CSS, Tailwind, and design skills.</p>
+</div>
+
+<div class="p-4 pt-8">
+	<h2 class="text-xl">Challenges</h2>
+	<ul>
+		<li>
+			<a href="/challenges/1">Challenge 1</a>
+		</li>
+		<li>
+			<a href="/challenges/2">Challenge 2</a>
+		</li>
+		<li>
+			<a href="/challenges/3">Challenge 3</a>
+		</li>
+	</ul>
 </div>
